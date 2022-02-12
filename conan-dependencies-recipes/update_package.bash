2562219d6cb5d621c63489ee69e41bb1b32e25a5
Dennis José da Silva <dennisjosesilva@gmail.com>
2022-02-11 23:57:36 -0300

    script to update conan package dependency.

---
+++ conan-dependencies-recipes/update_package.bash
@@ -0,0 +1,46 @@
+package_dirs=(
+  "./IcicleMorphotreeWidget"
+  "./morphotree"
+  "./ImageViewerWidget"
+  "./Skel"
+  "./Spline");
+
+package_name=(
+  "IcicleMorphotreeWidget"
+  "morphotree"
+  "ImageViewerWidget"
+  "Skel"
+  "Spline");
+
+package_conan=(
+  "IcicleMorphotreeWidget/0.0.1@dennisjosesilva/dev"
+  "morphotree/0.0.1@dennisjosesilva/dev"
+  "ImageViewerWidget/0.0.1@dennisjosesilva/dev"
+  "Skel/0.0.1@jieyingwang/dmd"
+  "Spline/0.0.1@jieyingwang/dmd"
+);
+
+echo "Packages to update:";
+echo "(1) IcicleMorphotreeWidget";
+echo "(2) morphotree";
+echo "(3) ImageViewerWidget";
+echo "(4) Skel";
+echo "(5) Spline";
+echo "Enter of the index of the package to be updated: ";
+read opt
+
+if [[ $opt -ge 1 ]] && [[ $opt -le 5 ]] 
+then
+  let package_index=opt-1
+  conan remove ${package_conan[$package_index]} --force;  
+  echo ""
+  echo "Package ${package_name[$package_index]} has been removed.";  
+  echo "=========================================================";
+  conan create ${package_dirs[$package_index]} ${package_conan[$package_index]};
+
+  echo "";
+  echo "=========================================================";
+  echo "Package ${package_name[$package_index]} has been updated.";
+else
+  echo "Index ${package_index} is Invalid.";
+fi
